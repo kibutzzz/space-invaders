b4fbfb1ca1edf8e997cde7763f0d44ad202d2938
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-12-11 13:48:35 -0200

    adicionado os event listeners para o movimentação da nave

--- main.js
+++ main.js
@@ -2,31 +2,41 @@ var nave;
 var inimigos = []
 var incremento = 1;
 var inc_inim = 2;
-function setup () {
+function setup() {
     var myCanvas = createCanvas(640, 480);
     myCanvas.parent("#wrapper");
-    
+
     nave = new Nave();
-    for (var i = 1; i <= 10; i ++){
-        for(var j = 1; j <= 4; j ++) {
+    for (var i = 1; i <= 10; i++) {
+        for (var j = 1; j <= 4; j++) {
             inimigos.push(new Inimigo(i * 58, j * 60));
         }
     }
-    
+
 }
 
-function draw () {
+function draw() {
     background(0, 0, 0);
-    
-    
-    if(nave.pos.x > width || nave.pos.x < 0) {
-        incremento *= -1;
-    }
-    nave.pos.x += incremento;
-    
+
     nave.desenha();
 
     inimigos.forEach((inimigo) => {
         inimigo.desenha();
     });
+
+    //verifica se alguma tecla esta pressionada
+    if (keyIsPressed) {
+        //se a seta para a direita estiver pressionada
+        if (keyCode == 39) {
+            nave.moverParaDireita();
+        }
+
+        //se a seta para a esquerda estiver pressionada
+        if (keyCode == 37) {
+            nave.moverParaEsquerda();
+        }
+
+    }
 }
+
+
